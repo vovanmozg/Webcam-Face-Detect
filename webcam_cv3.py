3ed7fa8defff5931fdff4e712ee2faf97ea85f6a
Vladimir Polukhin <vovanmozg@gmail.com>
2016-08-12 19:57:02 +0300

    first version of webcam face recognition

--- webcam_cv3.py
+++ webcam_cv3.py
@@ -2,6 +2,9 @@ import cv2
 import sys
 import logging as log
 import datetime as dt
+from skimage import io
+import time
+
 
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -26,17 +29,27 @@ while True:
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
+        output = frame[y:y+h, x:x+w] # Crop from x, y, w, h -> 100, 200, 300, 400
 
-    if anterior != len(faces):
-        anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+        fname = 'data/' + dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.jpg'
+        print(fname)
+
+        output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+        io.imsave(fname, output)
+
+    #if anterior != len(faces):
+    #    anterior = len(faces)
+    #    log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
 
     # Display the resulting frame
-    # cv2.imshow('Video', frame)
+    #cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    time.sleep(1) # delays for 5 seconds
 
 # When everything is done, release the capture
 video_capture.release()
